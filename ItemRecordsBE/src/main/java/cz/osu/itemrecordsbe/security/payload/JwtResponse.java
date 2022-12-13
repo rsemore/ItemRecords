a871c537af028f37115ab3ccac8c9b154d95de8b
Rosemore <99882603+rsemore@users.noreply.github.com>
2022-12-13 19:06:13 +0100

    User SignUp/SignIn using JWT

--- ItemRecordsBE/src/main/java/cz/osu/itemrecordsbe/security/payload/JwtResponse.java
+++ ItemRecordsBE/src/main/java/cz/osu/itemrecordsbe/security/payload/JwtResponse.java
@@ -13,15 +13,15 @@ public class JwtResponse {
     private String token;
     @Value("${application.jwt.token-prefix}")
     private String type;
-    private Long id;
+    private Long userId;
     private String username;
     private String email;
     private List<String> items;
     private List<String> comments;
 
-    public JwtResponse(String token, Long id, String username, String email, List<String> items, List<String> comments) {
+    public JwtResponse(String token, Long userId, String username, String email, List<String> items, List<String> comments) {
         this.token = token;
-        this.id = id;
+        this.userId = userId;
         this.username = username;
         this.email = email;
         this.items = items;
